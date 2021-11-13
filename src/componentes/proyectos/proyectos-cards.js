396d23c0631d9bc0993a142ff1d64d9e23fed9ac
Luana Vallejos <luanalorenavallejos@gmail.com>
2021-11-13 20:27:59 -0300

    correcion de warning

--- src/componentes/proyectos/proyectos-cards.js
+++ src/componentes/proyectos/proyectos-cards.js
@@ -6,7 +6,7 @@ const ProyectosCards = ({proyecto}) =>{
         <div className="proyectos-cards">
             <div className="proyectos-info">
                 <label className="proyectos-titulo">{proyecto.titulo}</label>
-                <div className className="proyecto-links">
+                <div className="proyecto-links">
                     {proyecto.link &&  (<a className="proyecto-link" href={proyecto.link}>
                             <div className="proyecto-boton">
                                 <i class="bi bi-globe2"></i> Link
