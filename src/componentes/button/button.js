18faa2c2a66ec41ae8fc0acb242a77d5c91f59b7
Luana Vallejos <luanalorenavallejos@gmail.com>
2021-11-19 13:08:27 -0300

    agrego funcionalidad a botones

--- src/componentes/button/button.js
+++ src/componentes/button/button.js
@@ -3,7 +3,7 @@ import './button.css';
 
 const CustomButton = ({texto,link}) => {
     return (
-        <button className="customButton" href={link}>{texto}</button>
+        <button className="customButton"><a href={link}>{texto}</a></button>
     )
 } 
 
--- src/componentes/home/home.js
+++ src/componentes/home/home.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './home.css';
+import axios from 'axios';
 import {Container,Row,Col} from 'react-bootstrap';
 import lulu from '../media/img-lulu.svg'
 import CustomButton from '../button/button'
@@ -8,6 +9,14 @@ import Proyectos from '../proyectos/proyectos'
 import Contacto from '../contacto/contacto'
 
 const Home = () => {
+    const [informacion, setInformacion] = useState({});
+    useEffect(()=>{
+        axios.get('https://portfolio-backend-lulu.herokuapp.com/profile')
+        .then(
+            res => setInformacion(res.data[0])
+        )
+    },[setInformacion])
+    
     return(
         <div className="home">
             <Container className="homee" >
@@ -19,12 +28,11 @@ const Home = () => {
                     </Col>
                     <Col sm={8} className="intro">
                     <h2>¡Hola! Soy</h2>
-                    <h1 className="titulo">Luana Vallejos</h1>
-                        <p className="descripcion">Desarrolladora Fullstack JS, estudiante de Informática y fan de los gatitos 💖</p>
+                    <h1 className="titulo">{informacion.nombre} {informacion.apellido}</h1>
+                        <p className="descripcion">{informacion.descripcion}</p>
                         <div className="botones">
                             <CustomButton texto="Contacto" link="#contacto"/>
-
-                            <CustomButton texto="Descargar CV"/>
+                            <CustomButton texto="Descargar CV" link="https://drive.google.com/file/d/19Oh8kFm2rWdtmCp8KWS0coCVcR1OyCEX/view?usp=sharing"/>
                         </div>
                         
                     </Col>
